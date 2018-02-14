c14c8f00831f91fbd41c688443ced1da37b484fa
josephsoliday <joseph.soliday@smaato.com>
2018-02-14 15:06:55 +0100

    - Example for team.

--- src/test/java/com/solid/mapper/custom/CustomMapper.java
+++ src/test/java/com/solid/mapper/custom/CustomMapper.java
@@ -1,5 +1,6 @@
 package com.solid.mapper.custom;
 
+import com.solid.mapper.Mapper;
 import com.solid.mapper.MapperType;
 import com.solid.mapper.ObjectMapper;
 import com.solid.mapping.MappingType;
@@ -9,8 +10,8 @@ import com.solid.mapping.annotation.Mapping;
 @Mapping(source="convertValue", customSourceConverter="java.lang.Integer.valueOf", destination="convertValue", customDestinationConverter="java.lang.String.valueOf", type=MappingType.BI_DIRECTIONAL)
 //@Mapping(source="element.value", destination="value", type=MappingType.BI_DIRECTIONAL)
 //@Mapping(source="element.checked", destination="checked", type=MappingType.BI_DIRECTIONAL)
-public class CustomMapper extends ObjectMapper {
-	public CustomMapper(MapperType type) {
+public class CustomMapper extends ObjectMapper implements Mapper {
+	public CustomMapper(final MapperType type) {
 		super(EntityClass.class, DtoClass.class, type);
 	}
 }
